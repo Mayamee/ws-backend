7e0d2eba44f746fb2966f7f9c1dad81c4ddbc0ed
Mayamee <ponyashcat228@gmail.com>
2023-08-12 22:47:40 +0300

    добавил waitForReadyState

--- app/services/MessageService/MessageService.ts
+++ app/services/MessageService/MessageService.ts
@@ -38,31 +38,25 @@ class MessageService {
       JSON.stringify(data)
     );
   };
-
-  public getMessages = async () => {
+  private waitForReadyState = async () => {
     if (this.state === MessageServiceState.initializing) {
-      await new Promise((resolve) => {
+      await new Promise<void>((resolve) => {
         const interval = setInterval(() => {
           if (this.state === MessageServiceState.ready) {
             clearInterval(interval);
-            resolve(this.messages);
+            resolve();
           }
         }, 100);
       });
     }
+  };
+
+  public getMessages = async () => {
+    await this.waitForReadyState();
     return this.messages;
   };
   public getMessage = async (id: string) => {
-    if (this.state === MessageServiceState.initializing) {
-      await new Promise((resolve) => {
-        const interval = setInterval(() => {
-          if (this.state === MessageServiceState.ready) {
-            clearInterval(interval);
-            resolve(this.messages.find((message) => message.id === id));
-          }
-        }, 100);
-      });
-    }
+    await this.waitForReadyState();
     return this.messages.find((message) => message.id === id);
   };
   public addMessage = async (messageContent: string): Promise<Message> => {
@@ -76,16 +70,21 @@ class MessageService {
   };
 
   public deleteMessage = async (id: string): Promise<void> => {
+    await this.waitForReadyState();
     this.messages = this.messages.filter((message) => message.id !== id);
     await this.saveMessages({ messages: this.messages });
   };
 
   public updateMessage = async (message: Message): Promise<void> => {
+    await this.waitForReadyState();
     this.messages = this.messages.map((m) =>
       m.id === message.id ? message : m
     );
     await this.saveMessages({ messages: this.messages });
   };
+  get currentState(): MessageServiceState {
+    return this.state;
+  }
 }
 
 const messageService = new MessageService(new FSService("messages"));
